be78215d5bb8ef1cedff533d3706aef25e7d25ad
wddufo <tian.li@ntnu.no>
2019-11-25 15:36:11 +0100

    change name to combustionFoam

--- combustionFoam.C
+++ combustionFoam.C
@@ -22,7 +22,7 @@ License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 Application
-    reactingParcelFoam
+    combustionFoam
 
 Description
     Transient solver for compressible, turbulent flow with a reacting,
